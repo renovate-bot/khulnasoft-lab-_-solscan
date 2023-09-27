1bf3e0817f85d834d6ffc1547dce7d61e322610f
Md Sulaiman <51925710+sulaiman-coder@users.noreply.github.com>
2023-09-27 12:01:06 +0600

    Create all_exceptions.py

---
+++ solscan/all_exceptions.py
@@ -0,0 +1,8 @@
+"""
+This module import all solscan exceptions
+"""
+# pylint: disable=unused-import
+from solscan.slithir.exceptions import SlithIRError
+from solscan.solc_parsing.exceptions import ParsingError, VariableNotFound
+from solscan.core.exceptions import SolscanCoreError
+from solscan.exceptions import SolscanException
